bd090120cd365e06909a03f2db4d8296cf9e8e2a
Liki Du <liki@likidu.com>
2022-11-13 19:52:52 -0800

    Add 2.5 manifest

---
+++ src/app/components/ExtraList.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import { push } from 'svelte-spa-router';
+  
+    import ListItem from '../../ui/components/list/ListItem.svelte';
+    import ListHeader from '../../ui/components/list/ListHeader.svelte';
+  
+    import type { TopList } from '../models';
+  
+    export let list: TopList;
+  
+    const { data } = list;
+    // Only use first two lists: Hot and Skyrocket
+    data.pop();
+  </script>
+  
+  {#each data as topList}
+    {#if topList.category === 'HOT_EPISODES_IN_24_HOURS'}
+      <ListHeader title="24 Hours Hot" />
+    {:else if topList.category === 'SKYROCKET_EPISODES'}
+      <ListHeader title="Skyrocket" />
+    {:else}
+      <ListHeader title="New Stars" />
+    {/if}
+    {#each topList.items as list, i}
+      <ListItem
+        imageUrl={list.item.podcast.image.thumbnailUrl}
+        primaryText={list.item.title}
+        secondaryText={list.item.podcast.title}
+        navi={{ itemId: `${topList.category}_${i + 1}`, onSelect: () => push(`/episode/${list.item.eid}`) }}
+      />
+    {/each}
+  {/each}
+  
